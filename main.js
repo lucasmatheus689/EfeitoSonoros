200a6b866d55c17564933e9e57697c6da182f3f1
lucasmatheus689 <110491879+lucasmatheus689@users.noreply.github.com>
2023-10-23 17:31:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 function tocarTeclaGrilo(){
 
-    document.querySelector('#som_tecla_grilo').play();
+    document.querySelector('.som_tecla_grilo').onclick =tocarTeclaGrilo ;
 }
+function tocarTrombetas(){
+    document.querySelector('som_tecla_trombeta').onclick =tocarTrombetas;
+}
+function tocarVaia(){
+    document.querySelector('som_tecla_vaia').onclick = tocarVaia;
+}
+function tocarRisada(){
+    document.querySelector('som_tecla_risada').onclick = tocarRisada;
+}
+
